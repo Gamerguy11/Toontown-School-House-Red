a051607e5a9e3e2e7bb39817f5a13be665f090df
RedDragon-11 <mgaddis97@gmail.com>
2022-07-07 17:27:18 -0400

    BattleCalulatorAI: Fixed Framework

--- toontown/battle/BattleCalculatorAI.py
+++ toontown/battle/BattleCalculatorAI.py
@@ -816,6 +816,11 @@ class BattleCalculatorAI:
 
     def __processBonuses(self, hp=1):
         lureTreeBonus = False
+        organicBonus = 0 # Will Add Org Lure Check Later
+
+        if organicBonus > 0:
+            lureTreeBonus = True
+
         if hp:
             bonusList = self.hpBonuses
             self.notify.debug('Processing hpBonuses: ' + repr(self.hpBonuses))
@@ -838,12 +843,12 @@ class BattleCalculatorAI:
                         attack[TOON_HPBONUS_COL] = math.ceil(totalDmgs * (self.DamageBonuses[numDmgs - 1] * 0.01))
                         if self.notify.getDebug():
                             self.notify.debug('Applying hp bonus to track ' + str(attack[TOON_TRACK_COL]) + ' of ' + str(attack[TOON_HPBONUS_COL]))
-                    elif len(attack[TOON_KBBONUS_COL]) > tgtPos:
-                        attack[TOON_KBBONUS_COL][tgtPos] = totalDmgs * 0.5
+                    elif len(attack[TOON_KBBONUS_COL]) > tgtPos and lureTreeBonus == True:
+                        attack[TOON_KBBONUS_COL][tgtPos] = totalDmgs * 0.65
                         if self.notify.getDebug():
                             self.notify.debug('Applying kb bonus to track ' + str(attack[TOON_TRACK_COL]) + ' of ' + str(attack[TOON_KBBONUS_COL][tgtPos]) + ' to target ' + str(tgtPos))
-                    elif len(attack[TOON_KBBONUS_COL]) > tgtPos and (lureTreeBonus == True):
-                        attack[TOON_KBBONUS_COL][tgtPos] = totalDmgs * 0.65
+                    elif len(attack[TOON_KBBONUS_COL]) > tgtPos:
+                        attack[TOON_KBBONUS_COL][tgtPos] = totalDmgs * 0.50
                         if self.notify.getDebug():
                             self.notify.debug('Applying kb bonus to track ' + str(attack[TOON_TRACK_COL]) + ' of ' + str(attack[TOON_KBBONUS_COL][tgtPos]) + ' to target ' + str(tgtPos))
                     else:
